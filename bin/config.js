0ce8750508dac815c44d1bb018eb7a8e703d0a06
Wacław Łabuda <waclaw.labuda@onebi.eu>
2017-09-14 14:10:16 +0200

    add config file

---
+++ bin/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // TCP port used by HTTP service
+    port: 3000,
+
+    // mongo url
+    mongoose: 'mongodb://localhost/clients-api'
+    //mongoose: 'mongodb://waclab1807:qwerty12345@ds135384.mlab.com:35384/clients-api'
+
+};
